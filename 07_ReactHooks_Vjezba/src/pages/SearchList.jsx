ef8691f032ef2d031bc883bde9e2af1e3b0ff954
David-Yuba <dado2012yuba@gmail.com>
2025-10-09 00:11:17 +0200

    SearchList exercise fixed so that it uses the useCallback function

--- 07_ReactHooks_Vjezba/src/pages/SearchList.jsx
+++ 07_ReactHooks_Vjezba/src/pages/SearchList.jsx
@@ -1,13 +1,16 @@
+import { useState, useCallback, useContext } from "react";
 
-import { useState, useCallback } from "react";
+import { ThemeContext } from "../components/ThemeContext";
 
 export default function SearchList(){
     const [names, setNames] = useState(["Ana", "Marko", "Ivana", "Petar", "Lana"]);
+    const [theme, setTheme] = useContext(ThemeContext);
 
     const [search, setSearch] = useState("");
     const fil = useCallback(filterNames, [search, names])
 
     function filterNames(){
+        console.log("I ran");
         return names.filter(n => n.toLowerCase().includes(search.toLowerCase()));
     }
 
@@ -16,7 +19,8 @@ export default function SearchList(){
             <h1>Search List</h1>
             <label>Search: </label>
             <input name = "search" value={search} onChange={(e) => setSearch(e.target.value)}/>
-            <NameList filteredNames={filterNames}></NameList>
+            <NameList filteredNames={fil}></NameList>
+            <button onClick={() => setTheme((value) => value === "dark" ? "light" : "dark")}>Change theme</button>
         </div>
     )
 }
